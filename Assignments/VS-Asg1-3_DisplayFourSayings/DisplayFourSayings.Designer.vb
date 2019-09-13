ee7df67d8fdfe8dc50e513af916791922d994dc9
davidane <54445028+davidane@users.noreply.github.com>
2019-09-13 08:54:26 -0600

    Editing labels and buttons
    Editing labels and buttons

---
+++ Assignments/VS-Asg1-3_DisplayFourSayings/DisplayFourSayings.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DisplayFourSayings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OwnerNameLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(690, 380)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(98, 58)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 307)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(191, 36)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 349)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(191, 36)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 391)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(191, 36)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 265)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(191, 36)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OwnerNameLabel
+        '
+        Me.OwnerNameLabel.AutoSize = True
+        Me.OwnerNameLabel.Location = New System.Drawing.Point(273, 425)
+        Me.OwnerNameLabel.Name = "OwnerNameLabel"
+        Me.OwnerNameLabel.Size = New System.Drawing.Size(126, 13)
+        Me.OwnerNameLabel.TabIndex = 5
+        Me.OwnerNameLabel.Text = "Created by: Dane Davids"
+        '
+        'DisplayFourSayings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.OwnerNameLabel)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "DisplayFourSayings"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OwnerNameLabel As Label
+End Class
---
+++ Assignments/VS-Asg1-3_DisplayFourSayings/DisplayFourSayings.vb
@@ -0,0 +1,14 @@
+﻿'Asg1-3
+'RCET 0265
+'Dane Davids
+'Pressing different buttons will present select sayings via labels
+
+
+Public Class DisplayFourSayings
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles _
+                        ExitButton.Click
+        Me.Close()
+    End Sub
+
+
+End Class
