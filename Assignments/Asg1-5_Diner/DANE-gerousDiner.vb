18eee3e8a868681b78e8e66385a4a2f2e49eaf88
davidane <54445028+davidane@users.noreply.github.com>
2019-09-16 08:58:48 -0600

    Added a new example and edited diner solution
    This example was created to demonstrate various methods to casing of phrases; example of constants and converting. Boolean was added to the diner solution so that each special was added only one time and would not be repeated.

--- Assignments/Asg1-5_Diner/DANE-gerousDiner.vb
+++ Assignments/Asg1-5_Diner/DANE-gerousDiner.vb
@@ -9,6 +9,9 @@ Option Strict On
 Public Class DinerForm
     Dim Special As String 'Dimming the special in the class portion will' _
     'make it possible for me to change its value over the whole form'
+    Dim SSpecial As Boolean
+    Dim Cspecial As Boolean
+    Dim FSpecial As Boolean
 
     Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles _
             SoupButton.Click
@@ -26,18 +29,23 @@ Public Class DinerForm
     End Sub
 
     Sub Soup()
-
+        If SSpecial = False Then SpecialLabel.Text = SpecialLabel.Text & " Soup: Cream of potato and bacon."
+        SSpecial = True
     End Sub
     Sub Chef()
-
+        If Cspecial = False Then SpecialLabel.Text = SpecialLabel.Text & " Chef's: Italian BMT with pepperjack cheese."
+        Cspecial = True
     End Sub
     Sub Fish()
-
+        If FSpecial = False Then SpecialLabel.Text = SpecialLabel.Text & " Fish: King(Chinook) Salmon Steak w/ one side."
+        FSpecial = True
     End Sub
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles _
             ExitButton.Click
         Me.Close()
     End Sub
+
+
 
     'Private Sub DinerForm_Activated(sender As Object, e As EventArgs) Handles Me.Activated
 
