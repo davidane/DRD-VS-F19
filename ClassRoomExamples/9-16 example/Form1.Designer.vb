2dff4272acd51e1aa9fc988bd624ab5d8ad384c6
davidane <54445028+davidane@users.noreply.github.com>
2019-09-17 08:10:16 -0600

    constants and variable
    experimenting with constants and variables

---
+++ ClassRoomExamples/9-16 example/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RunButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RunButton
+        '
+        Me.RunButton.Location = New System.Drawing.Point(277, 110)
+        Me.RunButton.Name = "RunButton"
+        Me.RunButton.Size = New System.Drawing.Size(239, 158)
+        Me.RunButton.TabIndex = 0
+        Me.RunButton.Text = "&Run"
+        Me.RunButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(667, 353)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(64, 36)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.RunButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.RunButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RunButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ ClassRoomExamples/9-16 example/Form1.vb
@@ -0,0 +1,49 @@
+﻿Option Strict On
+Option Explicit On
+
+
+Public Class Form1
+
+    'PascalCase FirstLetterOfEachWord
+    'Objects
+
+    'camelCase
+    'variablesUseCamelCase
+
+    'ALLCAPS
+    'CONSTANTS
+
+    'snake_case
+    'used in python have not seen on .NET
+
+    Sub VariablesAndConstants()
+
+        'variables can be changed
+        Dim someNumber As Integer
+        Dim result As Integer
+
+        'constants can not change
+        Const SOMECONST = 7D
+        Console.WriteLine("SOMECONST is... " & SOMECONST)
+
+        someNumber = 5
+        Console.WriteLine("some number is... " & someNumber)
+        'somenumber = 6
+        'Console.WriteLine(somenumber)
+
+        result = someNumber + CInt(SOMECONST)
+        Console.WriteLine("The result sum of somenumber and the constant is... " & result)
+
+
+
+    End Sub
+
+    Private Sub RunButton_Click(sender As Object, e As EventArgs) Handles RunButton.Click
+        VariablesAndConstants()
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
