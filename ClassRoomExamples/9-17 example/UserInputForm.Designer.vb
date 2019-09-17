7e07d4eb49042d821617478f884e671603431376
davidane <54445028+davidane@users.noreply.github.com>
2019-09-17 08:57:43 -0600

    Experimented with userinput and math; try/catch/finally error placing and debugging
    Experimented with userinput and math; try/catch/finally error placing and debugging

---
+++ ClassRoomExamples/9-17 example/UserInputForm.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserInputForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UserInputTxtBox = New System.Windows.Forms.TextBox()
+        Me.GetInputBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.EnterNumberlbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'UserInputTxtBox
+        '
+        Me.UserInputTxtBox.Location = New System.Drawing.Point(169, 100)
+        Me.UserInputTxtBox.Name = "UserInputTxtBox"
+        Me.UserInputTxtBox.Size = New System.Drawing.Size(191, 20)
+        Me.UserInputTxtBox.TabIndex = 0
+        '
+        'GetInputBtn
+        '
+        Me.GetInputBtn.Location = New System.Drawing.Point(88, 147)
+        Me.GetInputBtn.Name = "GetInputBtn"
+        Me.GetInputBtn.Size = New System.Drawing.Size(75, 23)
+        Me.GetInputBtn.TabIndex = 1
+        Me.GetInputBtn.Text = "Get Input"
+        Me.GetInputBtn.UseVisualStyleBackColor = True
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitBtn.Location = New System.Drawing.Point(713, 415)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(75, 23)
+        Me.ExitBtn.TabIndex = 2
+        Me.ExitBtn.Text = "E&xit"
+        Me.ExitBtn.UseVisualStyleBackColor = True
+        '
+        'EnterNumberlbl
+        '
+        Me.EnterNumberlbl.AutoSize = True
+        Me.EnterNumberlbl.Location = New System.Drawing.Point(84, 100)
+        Me.EnterNumberlbl.Name = "EnterNumberlbl"
+        Me.EnterNumberlbl.Size = New System.Drawing.Size(79, 13)
+        Me.EnterNumberlbl.TabIndex = 3
+        Me.EnterNumberlbl.Text = "Enter a number"
+        '
+        'UserInputForm
+        '
+        Me.AcceptButton = Me.GetInputBtn
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitBtn
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.EnterNumberlbl)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.GetInputBtn)
+        Me.Controls.Add(Me.UserInputTxtBox)
+        Me.Name = "UserInputForm"
+        Me.Text = "UserInput"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UserInputTxtBox As TextBox
+    Friend WithEvents GetInputBtn As Button
+    Friend WithEvents ExitBtn As Button
+    Friend WithEvents EnterNumberlbl As Label
+End Class
---
+++ ClassRoomExamples/9-17 example/UserInputForm.vb
@@ -0,0 +1,42 @@
+﻿Option Explicit On 'forces the compiler to make sure programmer is naming _ 
+'variables
+Option Strict On '
+
+Public Class UserInputForm
+    Private Sub GetInputBtn_Click(sender As Object, e As EventArgs) Handles _
+            GetInputBtn.Click
+
+        Dim firstNumber As Integer 'variables are in camelCase
+        Dim secondNumber As Integer = 7 'variables are in camelCase
+        Dim resultNumber As Integer 'variables are in camelCase
+
+        Try
+            'firstNumber = CInt(UserInputTxtBox.Text)
+            firstNumber = Integer.Parse(UserInputTxtBox.Text) '2 ways to convert
+            Console.WriteLine("In the try code... ")
+        Catch ex As Exception
+            Console.WriteLine("In the catch code... ")
+            MsgBox("Please enter a valid number")
+            UserInputTxtBox.Text = ""
+            UserInputTxtBox.Select()
+
+        Finally
+            Console.WriteLine("In the finally code... ")
+        End Try
+
+
+        resultNumber = firstNumber + secondNumber
+
+        UserInputTxtBox.Text = CStr(resultNumber)
+
+        Console.WriteLine("7 + " & UserInputTxtBox.Text & " = " &
+                          resultNumber & ".")
+
+    End Sub
+
+    Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles _
+            ExitBtn.Click
+        Me.Close()
+    End Sub
+
+End Class
