185aa1a031d5df2f17c40c03a7eb938b2ff27154
davidane <54445028+davidane@users.noreply.github.com>
2019-09-17 22:35:37 -0600

    asg2-5 inital setup
    deciding program flow and established some buttons

---
+++ Assignments/Asg2-5_addresslabel/Asg2-5_addresslabel/Asg2-5_AddressLabel.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AddressLabelForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DisplayBtn = New System.Windows.Forms.Button()
+        Me.ClearBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'DisplayBtn
+        '
+        Me.DisplayBtn.Location = New System.Drawing.Point(12, 363)
+        Me.DisplayBtn.Name = "DisplayBtn"
+        Me.DisplayBtn.Size = New System.Drawing.Size(189, 75)
+        Me.DisplayBtn.TabIndex = 0
+        Me.DisplayBtn.Text = "&Display"
+        Me.ToolTip1.SetToolTip(Me.DisplayBtn, "Display Filled Entry Boxes")
+        Me.DisplayBtn.UseVisualStyleBackColor = True
+        '
+        'ClearBtn
+        '
+        Me.ClearBtn.Location = New System.Drawing.Point(309, 363)
+        Me.ClearBtn.Name = "ClearBtn"
+        Me.ClearBtn.Size = New System.Drawing.Size(189, 75)
+        Me.ClearBtn.TabIndex = 1
+        Me.ClearBtn.Text = "&Clear"
+        Me.ToolTip1.SetToolTip(Me.ClearBtn, "Clear Boxes and Label")
+        Me.ClearBtn.UseVisualStyleBackColor = True
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.Location = New System.Drawing.Point(599, 363)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(189, 75)
+        Me.ExitBtn.TabIndex = 2
+        Me.ExitBtn.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitBtn, "Click to exit form")
+        Me.ExitBtn.UseVisualStyleBackColor = True
+        '
+        'AddressLabelForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.ClearBtn)
+        Me.Controls.Add(Me.DisplayBtn)
+        Me.Name = "AddressLabelForm"
+        Me.Text = "AddressLabel"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DisplayBtn As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents ClearBtn As Button
+    Friend WithEvents ExitBtn As Button
+End Class
