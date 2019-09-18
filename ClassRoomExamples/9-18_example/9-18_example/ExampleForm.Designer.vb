b3c286a61e25cb4c36a711eac60d382924ec8e93
davidane <54445028+davidane@users.noreply.github.com>
2019-09-18 08:45:08 -0600

    make validate numbers and displaying error message

---
+++ ClassRoomExamples/9-18_example/9-18_example/ExampleForm.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.RunBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(106, 53)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(304, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(106, 109)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(304, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(106, 161)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(304, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'RunBtn
+        '
+        Me.RunBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.RunBtn.Location = New System.Drawing.Point(106, 231)
+        Me.RunBtn.Name = "RunBtn"
+        Me.RunBtn.Size = New System.Drawing.Size(149, 81)
+        Me.RunBtn.TabIndex = 3
+        Me.RunBtn.Text = "&Run"
+        Me.RunBtn.UseVisualStyleBackColor = True
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.Location = New System.Drawing.Point(261, 231)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(149, 81)
+        Me.ExitBtn.TabIndex = 4
+        Me.ExitBtn.Text = "E&xit"
+        Me.ExitBtn.UseVisualStyleBackColor = True
+        '
+        'ExampleForm
+        '
+        Me.AcceptButton = Me.RunBtn
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitBtn
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.RunBtn)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "ExampleForm"
+        Me.Text = "Example Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents RunBtn As Button
+    Friend WithEvents ExitBtn As Button
+End Class
---
+++ ClassRoomExamples/9-18_example/9-18_example/ExampleForm.vb
@@ -0,0 +1,51 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class ExampleForm
+
+    Private Sub RunBtn_Click(sender As Object, e As EventArgs) Handles RunBtn.Click
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim errorMessage As String = ""
+
+
+        Try
+            'try something that might throw out 
+
+            secondNumber = CInt(TextBox2.Text)
+
+        Catch anyException As Exception
+            'exception thrown so do stuff
+            errorMessage = errorMessage & "Please enter a number in the second textbox." & vbNewLine
+
+            TextBox2.Select()
+            TextBox2.Clear()
+        Finally
+
+        End Try
+
+
+        Try
+            'try something that might throw out 
+            firstNumber = CInt(TextBox1.Text)
+        Catch anyException As Exception
+            'exception thrown so do stuff
+            errorMessage = errorMessage & "Please enter a number in the first textbox." & vbNewLine
+
+            TextBox1.Select()
+            TextBox1.Clear()
+        Finally
+
+        End Try
+
+        If errorMessage <> "" Then
+            MessageBox.Show(errorMessage, "We have problem")
+        End If
+
+    End Sub
+
+    Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
+        Me.Close()
+    End Sub
+End Class
