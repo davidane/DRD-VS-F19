bba2c0751ac7b2e1a356decc25a9e377fe3502cd
davidane <54445028+davidane@users.noreply.github.com>
2019-09-24 13:52:04 -0600

    Comparing strings and numbers using ifs/else

---
+++ ClassRoomExamples/9-23_Example/9-24_Example/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GoBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'GoBtn
+        '
+        Me.GoBtn.Location = New System.Drawing.Point(12, 339)
+        Me.GoBtn.Name = "GoBtn"
+        Me.GoBtn.Size = New System.Drawing.Size(175, 99)
+        Me.GoBtn.TabIndex = 0
+        Me.GoBtn.Text = "Go"
+        Me.GoBtn.UseVisualStyleBackColor = True
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.Location = New System.Drawing.Point(594, 339)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(194, 99)
+        Me.ExitBtn.TabIndex = 1
+        Me.ExitBtn.Text = "Exit"
+        Me.ExitBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.GoBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GoBtn As Button
+    Friend WithEvents ExitBtn As Button
+End Class
---
+++ ClassRoomExamples/9-23_Example/9-24_Example/Form1.vb
@@ -0,0 +1,45 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Text 'binary is default
+
+Public Class Form1
+    Private Sub GoBtn_Click(sender As Object, e As EventArgs) Handles GoBtn.Click
+        compareStrings()
+    End Sub
+
+    Sub compareNumbers()
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim thirdNumber As Integer
+
+        firstNumber = 1
+        secondNumber = 4
+        thirdNumber = 3
+
+        'is third number between first and second number
+        If thirdNumber > firstNumber And thirdNumber < secondNumber Then
+            MessageBox.Show("True")
+        Else
+            MessageBox.Show("False")
+        End If
+    End Sub
+
+    Sub compareStrings()
+        Dim firstString As String = "abcd"
+        Dim secondString As String = "abcde"
+        Dim x As String '= Hex(90)
+        x = Chr(90)
+
+
+
+        Console.WriteLine(firstString > secondString)
+        Console.WriteLine(x)
+
+
+    End Sub
+
+    Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
+        Me.Close()
+    End Sub
+End Class
