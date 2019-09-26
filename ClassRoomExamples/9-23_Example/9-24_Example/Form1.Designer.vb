c27e91185693a121f2e3a253e5e471689aebe516
davidane <54445028+davidane@users.noreply.github.com>
2019-09-25 18:16:14 -0600

    Demonstrated comparison more in depth with strings

--- ClassRoomExamples/9-23_Example/9-24_Example/Form1.Designer.vb
+++ ClassRoomExamples/9-23_Example/9-24_Example/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.GoBtn = New System.Windows.Forms.Button()
         Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'GoBtn
@@ -44,19 +45,29 @@ Partial Class Form1
         Me.ExitBtn.Text = "Exit"
         Me.ExitBtn.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(165, 102)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ExitBtn)
         Me.Controls.Add(Me.GoBtn)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents GoBtn As Button
     Friend WithEvents ExitBtn As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- ClassRoomExamples/9-23_Example/9-24_Example/Form1.vb
+++ ClassRoomExamples/9-23_Example/9-24_Example/Form1.vb
@@ -1,4 +1,7 @@
-﻿Option Strict On
+﻿'Dane Davids
+'9-24 & 9-25 Example
+
+Option Strict On
 Option Explicit On
 Option Compare Text 'binary is default
 
@@ -26,20 +29,41 @@ Public Class Form1
     End Sub
 
     Sub compareStrings()
-        Dim firstString As String = "abcd"
+        Dim firstString As String = "     Hello      "
         Dim secondString As String = "abcde"
-        Dim x As String '= Hex(90)
-        x = Chr(90)
+        Dim x As Integer '= Hex(90)
+        'x = Chr(90)
+        x = Len(firstString)
 
+        Console.WriteLine("Before trim " & firstString)
+        firstString = Trim(firstString)
+        Console.WriteLine("After trim " & firstString)
 
+        Console.WriteLine("before the loop" & Str(x))
+        For i = 1 To 10
+            firstString &= "!"
+            Console.WriteLine(firstString)
+        Next
+        x = Len(firstString)
+        Console.WriteLine("After the loop" & Str(x))
+        Console.WriteLine(firstString)
 
-        Console.WriteLine(firstString > secondString)
-        Console.WriteLine(x)
+        Console.WriteLine(Mid(firstString, 2, 3))
 
+        'Console.WriteLine(firstString > secondString)
+        'Console.WriteLine(x)
 
     End Sub
+
+    'Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+    '    Console.WriteLine(e.KeyCode & vbNewLine & e.KeyData & vbNewLine & e.KeyValue & vbNewLine & e.Shift)
+
+    'End Sub
 
     Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
         Me.Close()
     End Sub
+
 End Class
+
+
